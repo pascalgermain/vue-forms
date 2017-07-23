01c1baedaac37beef2daa7f751c077ef0f9f26e7
Pascal Germain <pascalgermain@me.com>
2017-07-23 21:50:40 +0200

    Add VueFormGenerator

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import 'onsenui/css/onsen-css-components.css'
 
 import Vue from 'vue'
 import VueOnsen from 'vue-onsenui'
+import VueFormGenerator from 'vue-form-generator'
 import App from './App'
 
 Vue.config.productionTip = false
 
 Vue.use(VueOnsen)
+Vue.use(VueFormGenerator)
 
 /* eslint-disable no-new */
 new Vue({
